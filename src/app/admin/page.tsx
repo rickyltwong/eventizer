007d6b75a02fcf71daf32d1f18ad8b9d3b73acc9
Ricky Wong <rickywonglt15@outlook.com>
2024-05-30 05:11:51 +0000

    Added starting folder structure and files

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function page(props: any) {
+  return (
+    <>
+      <h1>Admin Home Page</h1>
+    </>
+  );
+}
---
+++ src/app/users/[userid]/history/page.tsx
@@ -0,0 +1,3 @@
+export default function page(props: any) {
+  return <></>;
+}
