78e5b118d16214d6a7013dba351a9c8741750912
Ricky LT Wong <rickywonglt15@outlook.com>
2024-06-18 21:37:08 -0400

    Fixed lint errors

--- src/components/Surface/Surface.tsx
+++ src/components/Surface/Surface.tsx
@@ -1,5 +1,27 @@
 'use client';
 
+// import {
+//   Box,
+//   BoxProps,
+//   createPolymorphicComponent,
+//   PaperProps,
+// } from '@mantine/core';
+// import { forwardRef, ReactNode } from 'react';
+
+// type SurfaceProps = { children: ReactNode } & BoxProps & PaperProps;
+
+// const Surface = createPolymorphicComponent<'div', SurfaceProps>(
+//   forwardRef<HTMLDivElement, SurfaceProps>(({ children, ...others }, ref) => (
+//     <Box component="div" {...others} ref={ref}>
+//       {children}
+//     </Box>
+//   ))
+// );
+
+// Surface.displayName = 'Surface';
+
+// export default Surface;
+
 import {
   Box,
   BoxProps,
@@ -11,11 +33,18 @@ import { forwardRef, ReactNode } from 'react';
 type SurfaceProps = { children: ReactNode } & BoxProps & PaperProps;
 
 const Surface = createPolymorphicComponent<'div', SurfaceProps>(
-  forwardRef<HTMLDivElement, SurfaceProps>(({ children, ...others }, ref) => (
-    <Box component="div" {...others} ref={ref}>
-      {children}
-    </Box>
-  )),
+  forwardRef<HTMLDivElement, SurfaceProps>(function Surface(
+    { children, ...others },
+    ref
+  ) {
+    return (
+      <Box component="div" {...others} ref={ref}>
+        {children}
+      </Box>
+    );
+  })
 );
+
+Surface.displayName = 'Surface';
 
 export default Surface;
