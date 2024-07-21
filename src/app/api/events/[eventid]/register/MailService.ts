65f85336fd7bebaebc3048e4d056c15553daf89c
Kajanan Sivarajah <31389028+kajanan16@users.noreply.github.com>
2024-07-21 17:01:39 -0400

    Email service integrated and sending ticket to email feature implemented

---
+++ src/app/api/events/[eventid]/register/MailService.ts
@@ -0,0 +1,16 @@
+import nodemailer from 'nodemailer';
+
+const email = process.env.EMAIL;
+const pass = process.env.EMAIL_PASS;
+export const transporter = nodemailer.createTransport({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+    user: email,
+    pass,
+  },
+});
+
+export const mailOptions = {
+  from: email,
+};
