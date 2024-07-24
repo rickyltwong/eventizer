19d6ac16f2fd283fdcddcfafc7a854a074b95171
Kajanan Sivarajah <31389028+kajanan16@users.noreply.github.com>
2024-07-24 11:13:57 -0400

    hard coded images is removed

--- src/app/events/[eventid]/page.tsx
+++ src/app/events/[eventid]/page.tsx
@@ -131,6 +131,7 @@ const Page = () => {
               >
                 <Image
                   src={
+                    event?.image ??
                     'https://images.unsplash.com/photo-1593811167562-9cef47bfc4d7?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
                   }
                   width={400}
